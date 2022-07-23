9d4464f4ce696d2af994d57372c71bc4b18ecdbf
robsonshockwave <robsonshockwavegithub@gmail.com>
2022-07-22 22:55:54 -0300

    refactor: convert codes es5 to es6

--- src/exemplo01.test.js
+++ src/exemplo01.test.js
@@ -1,22 +1,22 @@
-describe('addition', () => {
-  let calc = null;
+// describe('addition', () => {
+//   let calc = null;
 
-  beforeEach(() => {
-    const options = {
-      precision: 2,
-    };
+//   beforeEach(() => {
+//     const options = {
+//       precision: 2,
+//     };
 
-    calc = new Calculator(options);
-  });
+//     calc = new Calculator(options);
+//   });
 
-  it('add two positive numbers', () => {
-    const result = calc.add(1.3334, 4.5);
-    expect(result).toEqual(5.83);
-  });
+//   it('add two positive numbers', () => {
+//     const result = calc.add(1.3334, 4.5);
+//     expect(result).toEqual(5.83);
+//   });
 
-  it('add two negative numbers', () => {
-    const result = calc.add(-1.3334, -4.5);
+//   it('add two negative numbers', () => {
+//     const result = calc.add(-1.3334, -4.5);
 
-    expect(result).toEqual(-5.83);
-  });
-});
+//     expect(result).toEqual(-5.83);
+//   });
+// });
